0cac4db48c1c881442d9570df84748ef37e4ecf5
yuki-yamada-man <major.in.from@i.softbank.jp>
2021-11-20 04:04:30 +0000

    first

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book_new = Book.new(book_params)
+    @book_new.user_id = current_user.id
+    if @book_new.save
+      redirect_to books_path(@book_new.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
+  end
+
+  def index
+    @book_new = Book.new
+    @user = current_user
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@user.id),notice:"You have update book successfully!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books
+
+  validates :name, presence:true
+  validates :introduction, presence:true
+end
