962fb91d3c28913ddb315f9d016c1dc0d3c7d3d0
yuki-yamada-man <major.in.from@i.softbank.jp>
2021-11-24 09:39:49 +0000

    create_view

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books
+  
 
   validates :name, presence:true
   validates :introduction, presence:true
